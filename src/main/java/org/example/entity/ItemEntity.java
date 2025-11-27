9cfc8041fd6adeae2c16f01989352e9715e5cd06
sumudumudunayana <mudunayanamihigurathna@gmail.com>
2025-11-27 22:49:05 +0530

    item entity created

---
+++ src/main/java/org/example/entity/ItemEntity.java
@@ -0,0 +1,27 @@
+package org.example.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table( name = "item")
+public class ItemEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String itemName;
+    private String itemDescription;
+    private String itemCategory;
+    private Double itemCostPrice;
+    private Double itemSellingPrice;
+    private Double itemLabeledPrice;
+    private String itemCompany;
+    private String itemDistributor;
+}
