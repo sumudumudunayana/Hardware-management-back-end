3234c86a8bf952a1887d8f779efe9e050aaf484b
sumudumudunayana <mudunayanamihigurathna@gmail.com>
2025-12-05 22:32:49 +0530

    category dto added

---
+++ src/main/java/org/example/dto/CategoryDto.java
@@ -0,0 +1,16 @@
+package org.example.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class CategoryDto {
+    private Integer id;
+    private String categoryName;
+    private String categoryDescription;
+}
