0a4a115b9c84a657428bd7e5567009aa64e22a6d
sumudumudunayana <mudunayanamihigurathna@gmail.com>
2025-12-05 22:37:22 +0530

    company repository added

---
+++ src/main/java/org/example/repository/CompanyRepository.java
@@ -0,0 +1,7 @@
+package org.example.repository;
+
+import org.example.entity.CompanyEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CompanyRepository extends JpaRepository<CompanyEntity, Integer> {
+}
