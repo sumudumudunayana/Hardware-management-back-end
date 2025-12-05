e66536ea4664efe8f8374740fc3a957df5faa1ab
sumudumudunayana <mudunayanamihigurathna@gmail.com>
2025-12-05 22:34:43 +0530

    category controller added

---
+++ src/main/java/org/example/controller/CategoryController.java
@@ -0,0 +1,48 @@
+package org.example.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.dto.CategoryDto;
+import org.example.dto.ItemDto;
+import org.example.service.CategoryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/category")
+@RequiredArgsConstructor
+@CrossOrigin
+@Slf4j
+public class CategoryController {
+    final CategoryService service;
+
+    @GetMapping("/get-all")
+    public List<CategoryDto> getItem(){
+        return service.getAll();
+    }
+
+    @PostMapping("/add-category")
+    public void addCategory(@RequestBody CategoryDto categoryDto){
+        log.info("updated-> {}",categoryDto);
+        service.addCategory(categoryDto);
+    }
+
+    @GetMapping("/search-by-id/{id}")
+    public CategoryDto getCategoryById(@PathVariable Integer id){
+        return service.searchCategoryById(id);
+    }
+
+    @DeleteMapping("/delete-by-id/{id}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void deleteCategoryById(@PathVariable Integer id){
+        service.deleteCategoryById(id);
+    }
+
+    @PutMapping("/update-item")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void updateCategoryById(@RequestBody CategoryDto categoryDto){
+        service.updateCategoryById(categoryDto);
+    }
+}
