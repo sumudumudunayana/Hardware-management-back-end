608add3a70fd88a46ad33c21cc06dd43d9d86126
sumudumudunayana <mudunayanamihigurathna@gmail.com>
2025-12-06 22:19:10 +0530

    company service updated

--- src/main/java/org/example/service/CompanyService.java
+++ src/main/java/org/example/service/CompanyService.java
@@ -1,4 +1,17 @@
 package org.example.service;
 
+import org.example.dto.CompanyDto;
+
+import java.util.List;
+
 public interface CompanyService {
+    List<CompanyDto> getAll();
+
+    void addCompany(CompanyDto companyDto);
+
+    void deleteCompanyById(Integer id);
+
+    CompanyDto searchCompanyById(Integer id);
+
+    void updateCompanyById(CompanyDto companyDto);
 }
