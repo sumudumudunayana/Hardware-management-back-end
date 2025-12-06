f47f81dfa26e00689ee74f81c3972043fcb5e445
sumudumudunayana <mudunayanamihigurathna@gmail.com>
2025-12-06 22:20:48 +0530

    Distributor repository created

---
+++ src/main/java/org/example/repository/DistributorRepository.java
@@ -0,0 +1,7 @@
+package org.example.repository;
+
+import org.example.entity.DistributorEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DistributorRepository extends JpaRepository<DistributorEntity, Integer> {
+}
