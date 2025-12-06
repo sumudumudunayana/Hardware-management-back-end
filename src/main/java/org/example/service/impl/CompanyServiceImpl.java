f76c7dab848567a9e78ca108f241e0c800de6fb6
sumudumudunayana <mudunayanamihigurathna@gmail.com>
2025-12-06 22:19:27 +0530

    company service impl updated

--- src/main/java/org/example/service/impl/CompanyServiceImpl.java
+++ src/main/java/org/example/service/impl/CompanyServiceImpl.java
@@ -1,4 +1,51 @@
 package org.example.service.impl;
 
-public class CompanyServiceImpl {
+import lombok.RequiredArgsConstructor;
+import org.example.dto.CompanyDto;
+import org.example.entity.CompanyEntity;
+import org.example.repository.CompanyRepository;
+import org.example.service.CompanyService;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CompanyServiceImpl implements CompanyService {
+    private final CompanyRepository repository;
+    private final ModelMapper mapper;
+
+    @Override
+    public List<CompanyDto> getAll() {
+        List<CompanyDto> CopmpanyArrayList = new ArrayList<>();
+        repository.findAll().forEach(entity->{
+            CopmpanyArrayList.add(mapper.map(entity, CompanyDto.class));
+        });
+        return CopmpanyArrayList;
+    }
+
+    @Override
+    public void addCompany(CompanyDto companyDto) {
+        System.out.println(companyDto);
+        repository.save(mapper.map(companyDto, CompanyEntity.class));
+    }
+
+    @Override
+    public void deleteCompanyById(Integer id){
+        repository.deleteById(id);
+    }
+
+    @Override
+    public CompanyDto searchCompanyById(Integer id) {
+        return mapper.map(repository.findById(id),CompanyDto.class);
+
+
+    }
+
+    @Override
+    public void updateCompanyById(CompanyDto companyDto) {
+        repository.save(mapper.map(companyDto, CompanyEntity.class));
+    }
 }
