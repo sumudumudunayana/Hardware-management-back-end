0035bee632533a44b13b28fc798ae9c484ed21cb
sumudumudunayana <mudunayanamihigurathna@gmail.com>
2025-12-16 22:49:48 +0530

    customer service updated

--- src/main/java/org/example/service/CustomerService.java
+++ src/main/java/org/example/service/CustomerService.java
@@ -1,4 +1,17 @@
 package org.example.service;
 
+import org.example.dto.CustomerDto;
+
+import java.util.List;
+
 public interface CustomerService {
+    List<CustomerDto> getAll();
+
+    void addCustomer(CustomerDto customerDto);
+
+    void deleteCustomerById(Integer id);
+
+    CustomerDto searchCustomerById(Integer id);
+
+    void updateCustomerById(CustomerDto customerDto);
 }
