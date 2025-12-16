cf9274f824994ae10fcbe1fd296676c313f0160e
sumudumudunayana <mudunayanamihigurathna@gmail.com>
2025-12-16 22:49:05 +0530

    customer dto updated

--- src/main/java/org/example/dto/CustomerDto.java
+++ src/main/java/org/example/dto/CustomerDto.java
@@ -1,4 +1,17 @@
 package org.example.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class CustomerDto {
+    private Integer id;
+    private String customerName;
+    private Integer customerContactNumber;
+    private String customerEmail;
 }
