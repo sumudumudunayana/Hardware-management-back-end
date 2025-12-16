c4035135b6e0b2d20533c57224c1d81a6491ff0e
sumudumudunayana <mudunayanamihigurathna@gmail.com>
2025-12-16 22:49:59 +0530

    customer service impl updated

--- src/main/java/org/example/service/impl/CustomerServiceImpl.java
+++ src/main/java/org/example/service/impl/CustomerServiceImpl.java
@@ -1,4 +1,51 @@
 package org.example.service.impl;
 
-public class CustomerServiceImpl {
+import lombok.RequiredArgsConstructor;
+import org.example.dto.CustomerDto;
+import org.example.entity.CustomerEntity;
+import org.example.repository.CustomerRepository;
+import org.example.service.CustomerService;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerServiceImpl implements CustomerService {
+    private final CustomerRepository repository;
+    private final ModelMapper mapper;
+
+    @Override
+    public List<CustomerDto> getAll() {
+        List<CustomerDto> CustomerArrayList = new ArrayList<>();
+        repository.findAll().forEach(entity->{
+            CustomerArrayList.add(mapper.map(entity, CustomerDto.class));
+        });
+        return CustomerArrayList;
+    }
+
+    @Override
+    public void addCustomer(CustomerDto customerDto) {
+        System.out.println(customerDto);
+        repository.save(mapper.map(customerDto, CustomerEntity.class));
+    }
+
+    @Override
+    public void deleteCustomerById(Integer id){
+        repository.deleteById(id);
+    }
+
+    @Override
+    public CustomerDto searchCustomerById(Integer id) {
+        return mapper.map(repository.findById(id),CustomerDto.class);
+
+
+    }
+
+    @Override
+    public void updateCustomerById(CustomerDto customerDto) {
+        repository.save(mapper.map(customerDto, CustomerEntity.class));
+    }
 }
