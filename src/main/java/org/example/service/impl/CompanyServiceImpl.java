5c78392e230f7c9a6fc9a8072190d9749510af41
sumudumudunayana <mudunayanamihigurathna@gmail.com>
2026-01-25 22:42:50 +0530

    CompanyServiceImpl updated

--- src/main/java/org/example/service/impl/CompanyServiceImpl.java
+++ src/main/java/org/example/service/impl/CompanyServiceImpl.java
@@ -7,7 +7,6 @@ import org.example.repository.CompanyRepository;
 import org.example.service.CompanyService;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.List;
 
