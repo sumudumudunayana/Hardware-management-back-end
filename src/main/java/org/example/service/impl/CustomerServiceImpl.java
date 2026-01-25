13a27a9965adeb9a987c231423322da2d673b503
sumudumudunayana <mudunayanamihigurathna@gmail.com>
2026-01-25 22:44:06 +0530

    CustomerServiceImpl updated

--- src/main/java/org/example/service/impl/CustomerServiceImpl.java
+++ src/main/java/org/example/service/impl/CustomerServiceImpl.java
@@ -7,7 +7,6 @@ import org.example.repository.CustomerRepository;
 import org.example.service.CustomerService;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.List;
 
