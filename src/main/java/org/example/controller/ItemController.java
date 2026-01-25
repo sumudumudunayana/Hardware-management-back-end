1f6b37eeccfed8da48c1679c9e493891f9977203
sumudumudunayana <mudunayanamihigurathna@gmail.com>
2026-01-25 22:40:10 +0530

    ItemController updated

--- src/main/java/org/example/controller/ItemController.java
+++ src/main/java/org/example/controller/ItemController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @Slf4j
 public class ItemController {
     final ItemService service;
+
     @GetMapping("/get-all")
     public List<ItemDto> getItem(){
         return service.getAll();
